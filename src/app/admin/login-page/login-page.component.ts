96eb3473c365365608349ce00b41ffb399ac3eb9
OlgaGatalskaya <gatalolia86@gmail.com>
2020-06-02 14:34:55 +0200

    create form and validation

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
+  
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup
+  submited = false
+  
+  constructor() {
+   
+   }
 
   ngOnInit(): void {
+    this.form = new FormGroup ({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+    })
+    
+    
   }
-
+  
+  
+  submit(){}
+    
+  
 }
